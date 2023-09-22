2ef47dc6e755fccdb9ba3faba94af212fc1706c2
Yuhan Wang <wyuhan18@gmail.com>
2023-09-22 18:06:37 +0930

    menu update

--- BuyMenu.h
+++ BuyMenu.h
@@ -42,17 +42,31 @@ class BuyMenu {
     // setting buying items text
     plant_list.push_back(Text());
     plant_list[0].setFont(font);
-    plant_list[0].setString("Carrot seed");
+    plant_list[0].setString("carrot seed");
     plant_list[0].setCharacterSize(10);
     plant_list[0].setFillColor(Color::White);
     plant_list[0].setPosition(x + 10, 60);
 
     plant_list.push_back(Text());
     plant_list[1].setFont(font);
-    plant_list[1].setString("Berry plant seed");
+    plant_list[1].setString("potato seed");
     plant_list[1].setCharacterSize(10);
     plant_list[1].setFillColor(Color::White);
     plant_list[1].setPosition(x + 10, 80);
+
+    plant_list.push_back(Text());
+    plant_list[2].setFont(font);
+    plant_list[2].setString("strawberry seed");
+    plant_list[2].setCharacterSize(10);
+    plant_list[2].setFillColor(Color::White);
+    plant_list[2].setPosition(x + 10, 100);
+
+    plant_list.push_back(Text());
+    plant_list[3].setFont(font);
+    plant_list[3].setString("blueberry seed");
+    plant_list[3].setCharacterSize(10);
+    plant_list[3].setFillColor(Color::White);
+    plant_list[3].setPosition(x + 10, 120);
 
     background->setPosition(x, y);
     // turning on and off
@@ -65,7 +79,7 @@ class BuyMenu {
   void draw(RenderWindow* win) {
     win->draw(*background);
     win->draw(title);
-    for (int i = 0; i < 2; i++) {
+    for (int i = 0; i < 4; i++) {
       win->draw(plant_list[i]);
     }
   }

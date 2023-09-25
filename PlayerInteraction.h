c5df85262dbb221e821af540fc996cfcf7ed5b1f
georgiaMcl <a1882072@adelaide.edu.au>
2023-09-25 18:59:27 +0930

    YOU CAN PLANT PLANTS

--- PlayerInteraction.h
+++ PlayerInteraction.h
@@ -21,16 +21,10 @@ class PlayerInteraction {
         PlayerInteraction() {};
 
         void seedPlant(int seedType, Player p, std::vector<tile*>* backgroundTiles) { // takes in an int which represents the seed type, takes in an object player, and a pointer to a vector of pointers to tiles (a pointer to background tiles)
-            
+        
             // changes the x and y coordinates of the player to valid tile positions
             int player_x = (floor(p.get_x()/50)*50);
             int player_y = (floor(p.get_y()/50)*50);
-            
-            // int player_x = p.get_x()/50;
-            // player_x = player_x * 50;
-
-            // int player_y = p.get_y()/50;
-            // player_y = player_y * 50;
 
             for (int i = 0; i < 144; i++) {
                 tile* currentTile = (*backgroundTiles)[i]; // Access the current tile pointer in the vector
@@ -38,17 +32,56 @@ class PlayerInteraction {
                 // determine which tile the player is on
                 if (currentTile->get_x() == player_x && currentTile->get_y() == player_y){
                     if (currentTile->get_isPlantable() == true) {
-                        // switch (seedType) {
-                        //     case 1: // 1 is carrot seeds
-                                
+                        switch (seedType) {
+                            case 1: // type 1 is carrot 
+                            {
                                 // create a new carrot tile (in the heap) with cooridnates of player position and set carrot as a pointer to the carrot tile
                                 Carrot* carrot = new Carrot(player_x, player_y);
-                                
                                 // remove old tile from vector and replace with new carrot tile
                                 delete currentTile;
                                 // sets the current tile player is on to be a carrot tile
                                 (*backgroundTiles)[i] = carrot;
                                 break;
+                            }
+
+                            case 2: // type 2 is potato
+                            {
+                                // create a new potato tile (in the heap) with cooridnates of player position and set potato as a pointer to the potato tile
+                                Potato* potato = new Potato(player_x, player_y);
+                                // remove old tile from vector and replace with new carrot tile
+                                delete currentTile;
+                                // sets the current tile player is on to be a carrot tile
+                                (*backgroundTiles)[i] = potato;
+                                break;
+                            }
+
+                            case 3: // type 3 is blueberry
+                            {
+                                // create a new bluebbery tile (in the heap) with cooridnates of player position and set blueberry as a pointer to the blueberry tile
+                                Blueberry* blueberry = new Blueberry(player_x, player_y);
+                                // remove old tile from vector and replace with new carrot tile
+                                delete currentTile;
+                                // sets the current tile player is on to be a carrot tile
+                                (*backgroundTiles)[i] = blueberry;
+                                break;
+                            }
+
+                            case 4: // type 4 is strawberry
+                            {
+                                // create a new strawberry tile (in the heap) with cooridnates of player position and set strawberry as a pointer to the strawberry tile
+                                Strawberry* strawberry = new Strawberry(player_x, player_y);
+                                // remove old tile from vector and replace with new carrot tile
+                                delete currentTile;
+                                // sets the current tile player is on to be a carrot tile
+                                (*backgroundTiles)[i] = strawberry;
+                                break;
+                            }
+                            
+                            default:    
+                                std::cout<< "error: could not plant as type was invalid" << std::endl;
+
+                        }; 
+                        break; // stops looping through for loop
                     }
                 }
             }

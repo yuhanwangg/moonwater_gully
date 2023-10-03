3a8bf13c57723f15d775f78ceec2732839176c11
Yuhan Wang <wyuhan18@gmail.com>
2023-10-03 16:31:43 +1030

    mischa changes

--- Player.h
+++ Player.h
@@ -3,8 +3,15 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 using namespace sf;
+#include <cmath>
+
+#include "Blueberry.h"
+#include "Carrot.h"
 #include "Inventory.h"
 #include "Plant.h"
+#include "Player.h"
+#include "Potato.h"
+#include "Strawberry.h"
 // #include "PlayerInteraction.h"
 
 class Player {
@@ -22,7 +29,7 @@ class Player {
     x = _x;
     y = _y;
     shells = 500;
-    speed = 0.1;
+    speed = 2;
 
     body->setRadius(r);
     body->setFillColor(Color::Blue);
@@ -34,6 +41,103 @@ class Player {
     win->draw(*body);
     // inventory->drawInventory(win);
   }
+
+  // Seeding plant
+
+  void seedPlant(int seedType, std::vector<tile*>* backgroundTiles) {
+    int player_x = (floor(body->getPosition().x / 50) * 50);
+    int player_y = (floor(body->getPosition().y / 50) * 50);
+
+    for (int i = 0; i < 144; i++) {
+      tile* currentTile = (*backgroundTiles)[i];  // Access the current tile
+                                                  // pointer in the vector
+
+      // determine which tile the player is on
+      if (currentTile->get_x() == player_x &&
+          currentTile->get_y() == player_y) {
+        if (currentTile->get_isPlantable() == true) {
+          switch (seedType) {
+            case 2:  // type 2 is blueberry seeds in inventory position 2
+            {
+              // create a new bluebbery tile (in the heap) with cooridnates of
+              // player position and set blueberry as a pointer to the blueberry
+              // tile
+              Blueberry* blueberry = new Blueberry(player_x, player_y);
+              // remove old tile from vector and replace with new carrot tile
+              delete currentTile;
+              // sets the current tile player is on to be a carrot tile
+              (*backgroundTiles)[i] = blueberry;
+              break;
+            }
+
+            case 4:  // type 4 is strawberry seeds in inventory position 4
+            {
+              // create a new strawberry tile (in the heap) with cooridnates of
+              // player position and set strawberry as a pointer to the
+              // strawberry tile
+              Strawberry* strawberry = new Strawberry(player_x, player_y);
+              // remove old tile from vector and replace with new carrot tile
+              delete currentTile;
+              // sets the current tile player is on to be a carrot tile
+              (*backgroundTiles)[i] = strawberry;
+              break;
+            }
+
+            case 6:  // type 6 is potato seeds in inventory position 6
+            {
+              // create a new potato tile (in the heap) with cooridnates of
+              // player position and set potato as a pointer to the potato tile
+              Potato* potato = new Potato(player_x, player_y);
+              // remove old tile from vector and replace with new carrot tile
+              delete currentTile;
+              // sets the current tile player is on to be a carrot tile
+              (*backgroundTiles)[i] = potato;
+              break;
+            }
+
+            case 8:  // type 8 is carrot seeds in inventory position 8
+            {
+              // create a new carrot tile (in the heap) with cooridnates of
+              // player position and set carrot as a pointer to the carrot tile
+              Carrot* carrot = new Carrot(player_x, player_y);
+              // remove old tile from vector and replace with new carrot tile
+              delete currentTile;
+              // sets the current tile player is on to be a carrot tile
+              (*backgroundTiles)[i] = carrot;
+              break;
+            }
+
+            default:
+              std::cout << "error: could not plant as type was invalid"
+                        << std::endl;
+          };
+          break;  // stops looping through for loop
+        }
+      }
+    }
+    return;
+  }
+
+  // Water plant
+
+  void waterPlant(std::vector<tile*>* backgroundTiles) {
+    // changes the x and y coordinates of the player to valid tile positions
+    int player_x = (floor(body->getPosition().x / 50) * 50);
+    int player_y = (floor(body->getPosition().y / 50) * 50);
+
+    for (int i = 0; i < 144; i++) {
+      tile* currentTile = (*backgroundTiles)[i];  // Access the current tile
+                                                  // pointer in the vector
+
+      // determine which tile the player is on
+      if (currentTile->get_x() == player_x &&
+          currentTile->get_y() == player_y) {
+        (*backgroundTiles)[i]->set_hydrationLevel(1);
+        break;
+      }
+    }
+    return;
+  };
 
   // movement of the player
   void move_right() { body->move(speed, 0); }
